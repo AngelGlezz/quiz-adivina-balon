01cae8b826e5bf0ee52331530e6f372f4a85f61c
Angel <angelglezz.7@gmail.com>
2018-09-28 09:39:32 -0500

    versión juan

--- metas.php
+++ metas.php
@@ -3,16 +3,16 @@
 		"mal" => array(
 			"title" => "Adivina Juan",
 			"description" => "Claramente no sabes ni lo que es un balón… mejor dedícate al box, al póker o qué sé yo",
-			"image" => "images/Resultados-1.jpg" 
+			"image" => "images/resultados-1.jpg" 
 		),
 		"maso" => array(
 			"title" => "Adivina Juan",
 			"description" => "Mñeh, pudiste salir mejor. Ni muy muy, ni tan tan. Échale más ganitas. Sí se puede.",
-			"image" => "images/Resultados-2.jpg" 
+			"image" => "images/resultados-2.jpg" 
 		),
 		"bien" => array(
 			"title" => "Adivina Juan",
 			"description" => "¡Eres un D10S! ¡El balón y tú son uno mismo!",
-			"image" => "images/Resultados-3.jpg" 
+			"image" => "images/resultados-3.jpg" 
 		)
 	);
